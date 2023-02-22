2892f5ac09dc01dc7aaf339635b2cd9e8dfdfce7
Chris Oden <coden@its.jnj.com>
2023-02-22 15:25:05 -0500

    Remove bodiless

---
+++ sites/vital-stackbit/.stackbit/cache/stackbit.config.5IAQLYWC.cjs
@@ -0,0 +1,33 @@
+"use strict";
+function stackbit_process_cwd_shim() { return "/Users/coden/dxp/vital-stackbit/sites/vital-stackbit" }
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// stackbit.config.ts
+var stackbit_config_exports = {};
+__export(stackbit_config_exports, {
+  default: () => stackbit_config_default
+});
+module.exports = __toCommonJS(stackbit_config_exports);
+var stackbit_config_default = {
+  stackbitVersion: "~0.6.0",
+  ssgName: "gatsby"
+};
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {});
+//# sourceMappingURL=stackbit.config.5IAQLYWC.cjs.map
--- sites/vital-stackbit/gatsby-config.js
+++ sites/vital-stackbit/gatsby-config.js
@@ -4,7 +4,7 @@ const express = require('express');
 //   createDefaultContentPlugins,
 //   getSampleDefaultContentConfig,
 // } = require('@bodiless/gatsby-theme-bodiless/dist/DefaultContent');
-const { getDisabledPages } = require('@bodiless/components/node-api');
+// const { getDisabledPages } = require('@bodiless/components/node-api');
 // const getSSIEntities = require('@bodiless/gatsby-plugin-ssi/read-ssi-entities');
 // const {
 //   getConfig: getSiteDefaultContentConfig,
@@ -18,74 +18,74 @@ require('dotenv').config({
 
 const SITEURL = process.env.SITE_URL;
 
-const disablePageList = getDisabledPages();
-const disabledPages = Object.keys(disablePageList).filter(
-  item => disablePageList[item].pageDisabled === true || disablePageList[item].indexingDisabled,
-);
+// const disablePageList = getDisabledPages();
+// const disabledPages = Object.keys(disablePageList).filter(
+//   item => disablePageList[item].pageDisabled === true || disablePageList[item].indexingDisabled,
+// );
 
 // Gatsby plugins list.
 const plugins = [
-  {
-    resolve: 'gatsby-plugin-compile-es6-packages',
-    options: {
-      modules: ['@bodiless/gatsby-theme-bodiless'],
-    },
-  },
-  {
-    resolve: '@bodiless/gatsby-theme-bodiless',
-    options: {
-      gatsbyImage: {
-        sharpArgs: {
-          quality: 90,
-        },
-      },
-      // Static replacement options, see docs for more info.
-      static: {
-        // Pass false to disable static replacement. Defaults to true.
-        enabled: true,
-        // Static replacement works by searching for files ending in ".bl-edit" and replacing them
-        // with a file with the same name, but ending in ".static" instead. However, all files are
-        // skipped by default for compatibility reasons.
-        //
-        // You can pass a boolean to include or exclude all files easily. True means include all
-        // ".bl-edit" files, enabling static replacement on all packages. False disables static
-        // replacement, which is the same as passing `enabled: false` in the options object.
-        //
-        // You can also pass a RegExp which will be matched against the **absolute** path of every
-        // .bl-edit file being imported. If it matches and a sibling .static file is found, the file
-        // will be replaced. For instance, if you want to include all `vital-editors` and
-        // `vital-link` files, you may pass `/vital-editors|vital-link/` to this option.
-        //
-        // Defaults to false.
-        include: true,
-        // Pass false to disable logging all resolving operations. Defaults to true.
-        logging: true,
-      }
-    },
-  },
+  // {
+  //   resolve: 'gatsby-plugin-compile-es6-packages',
+  //   options: {
+  //     modules: ['@bodiless/gatsby-theme-bodiless'],
+  //   },
+  // },
+  // {
+  //   resolve: '@bodiless/gatsby-theme-bodiless',
+  //   options: {
+  //     gatsbyImage: {
+  //       sharpArgs: {
+  //         quality: 90,
+  //       },
+  //     },
+  //     // Static replacement options, see docs for more info.
+  //     static: {
+  //       // Pass false to disable static replacement. Defaults to true.
+  //       enabled: true,
+  //       // Static replacement works by searching for files ending in ".bl-edit" and replacing them
+  //       // with a file with the same name, but ending in ".static" instead. However, all files are
+  //       // skipped by default for compatibility reasons.
+  //       //
+  //       // You can pass a boolean to include or exclude all files easily. True means include all
+  //       // ".bl-edit" files, enabling static replacement on all packages. False disables static
+  //       // replacement, which is the same as passing `enabled: false` in the options object.
+  //       //
+  //       // You can also pass a RegExp which will be matched against the **absolute** path of every
+  //       // .bl-edit file being imported. If it matches and a sibling .static file is found, the file
+  //       // will be replaced. For instance, if you want to include all `vital-editors` and
+  //       // `vital-link` files, you may pass `/vital-editors|vital-link/` to this option.
+  //       //
+  //       // Defaults to false.
+  //       include: true,
+  //       // Pass false to disable logging all resolving operations. Defaults to true.
+  //       logging: true,
+  //     }
+  //   },
+  // },
   // {
   //   resolve: '@bodiless/gatsby-plugin-ssi',
   //   options: {
   //     ssiEntities: getSSIEntities('ssi/ssi_conf.json'),
   //   }
   // },
-  {
-    resolve: 'gatsby-plugin-canonical-urls',
-    options: {
-      siteUrl: SITEURL,
-    },
-  },
+  //  {
+  // .   resolve: 'gatsby-plugin-canonical-urls',
+  // .   options: {
+  // .     siteUrl: SITEURL,
+  // .   },
+  // },
   {
     resolve: 'gatsby-plugin-sitemap',
-    options: { excludes: disabledPages },
+    // options: { excludes: disabledPages },
   },
   // ...createDefaultContentPlugins(
   //   ...getSampleDefaultContentConfig(),
   //   ...getSiteDefaultContentConfig(),
   // ),
-  {
-    resolve: 'vital-stackbit',
-  }
+  // {
+  //   resolve: 'vital-stackbit',
+  // }
 ];
 
 const robotsTxtPolicy = [
